0fa864f1f2f79d15854cef3bf8230b1f01836243
Dajana747 <d.kockova@gmail.com>
2025-05-09 22:14:55 +0200

    oprava projektu

--- main.py
+++ main.py
@@ -43,74 +43,61 @@ separator = "-"*40
 user_name = input("Enter your username:")
 password = input("Enter your password:")
 
-if user_name in registered_users: 
-
-    if registered_users[user_name] == password:
-        print(separator)
-        print(f"Welcome to the app, {user_name.capitalize()}", "We have 3 texts to be analyzed.", sep="\n")
-        print(separator) 
-        entered_number = (input("Enter a number btw. 1 and 3 to select:"))
-
-        if not entered_number.isnumeric():
-            print("You need to write a number from 1 to 3!")
-
-        elif int(entered_number) in range(1,4):
-            print(separator)
-            chosen_text = TEXTS[int(entered_number)-1]
-            print("There are", len(chosen_text.split()), "words in the selected text.")
-
-            number_of_titles = int()
-            for title in chosen_text.split():
-                 if title.istitle():
-                      number_of_titles += 1
-            print("There are", number_of_titles, "titlecase words.")
-
-            number_of_uppers = int()
-            for uppers in chosen_text.split():
-                 if uppers.isupper():
-                    number_of_uppers += 1
-            print("There are", number_of_uppers, "uppercase words.")
-
-            number_of_lowers = int()
-            for lowers in chosen_text.split():
-                 if lowers.islower():
-                    number_of_lowers += 1
-            print("There are", number_of_lowers, "lowercase words.")
-
-            count_of_numbers = int()
-            numbers_list = list()
-            for numbers in chosen_text.split():
-                if numbers.isdigit():
-                    count_of_numbers += 1
-                    numbers_list.append(int(numbers))
-            print("There are", count_of_numbers, "numeric strings.")
-            print("The sum of all the numbers", sum(numbers_list))
-
-            print(separator)
-            print("LEN", " OCCURRENCES ", "NR.", sep="|")
-            print(separator)
-
-            length_of_words = list()
-
-            for word in chosen_text.split():
-                word = word.replace(".","").replace(",","")
-                length_of_words.append(len(word))
-
-            for number_of_letters in set(length_of_words):
-                occurrences = length_of_words.count(number_of_letters)
-                print(f"{number_of_letters}|{"*"*occurrences} |{occurrences}")
-
-        else:
-            print("You need to write a number from 1 to 3!")
-       
-    else:
-        print("Ooops! Wrong password, terminating the program..")
-
-else:
-    print("username:", user_name)
-    print("password:", password)
+if not user_name in registered_users: 
     print("unregistered user, terminating the program..")
 
+elif registered_users[user_name] != password:
+    print("Ooops! Wrong password, terminating the program..")
 
+else:
+    print(separator)
+    print(f"Welcome to the app, {user_name.capitalize()}", "We have 3 texts to be analyzed.", sep="\n")
+    print(separator)
+    entered_number = (input("Enter a number btw. 1 and 3 to select:"))
 
+    if not entered_number.isnumeric():
+        print("You need to write a number from 1 to 3!")
+
+    elif not int(entered_number) in range(1,4):
+        print("You need to write a number from 1 to 3!")
+    
+    else:
+        print(separator)
+        chosen_text = TEXTS[int(entered_number)-1].split()
+        print("There are", len(chosen_text), "words in the selected text.")
+        
+        number_of_titles = int()
+        number_of_uppers = int()
+        number_of_lowers = int()
+        count_of_numbers = int()
+        numbers_list = list()
+        for statistics in chosen_text:
+            if statistics.istitle():
+                number_of_titles += 1
+            elif statistics.isupper():
+                number_of_uppers += 1
+            elif statistics.islower():
+                number_of_lowers += 1
+            elif statistics.isdigit():
+                count_of_numbers += 1
+                numbers_list.append(int(statistics))
+        print("There are", number_of_titles, "titlecase words.")
+        print("There are", number_of_uppers, "uppercase words.")
+        print("There are", number_of_lowers, "lowercase words.")
+        print("There are", count_of_numbers, "numeric strings.")
+        print("The sum of all the numbers", sum(numbers_list))
+
+        print(separator)
+        print("LEN", " OCCURRENCES ", "NR.", sep="|")
+        print(separator)
+
+        length_of_words = list()
+
+        for word in chosen_text:
+            word = word.replace(".","").replace(",","")
+            length_of_words.append(len(word))
+
+        for number_of_letters in set(length_of_words):
+            occurrences = length_of_words.count(number_of_letters)
+            print(f"{number_of_letters}|{"*"*occurrences} |{occurrences}")
     
